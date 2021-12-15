683708d11dc3940511ede9a1e69aec116a892552
eljefe223 <jes@microsoft.com>
2021-12-15 09:30:04 -0800

    removed directional style sheet

--- packages/web-components/fast-components/src/search/search.styles.ts
+++ packages/web-components/fast-components/src/search/search.styles.ts
@@ -34,27 +34,7 @@ import {
     typeRampBaseFontSize,
     typeRampBaseLineHeight,
 } from "../design-tokens";
-import { DirectionalStyleSheetBehavior, heightNumber } from "../styles/index";
-
-/**
- * LTR styles for search
- * @internal
- */
-const ltrStyles = css`
-    .clear-button {
-        right: 1px;
-    }
-`;
-
-/**
- * RTL styles for search
- * @internal
- */
-const rtlStyles = css`
-    .clear-button {
-        left: 1px;
-    }
-`;
+import { heightNumber } from "../styles/index";
 
 const closeButtonHover = DesignToken.create<Swatch>("close-button-hover").withDefault(
     (target: HTMLElement) => {
@@ -288,6 +268,5 @@ export const searchStyles: (
                     color: ${SystemColors.GrayText};
                 }
             `
-        ),
-        new DirectionalStyleSheetBehavior(ltrStyles, rtlStyles)
+        )
     );
